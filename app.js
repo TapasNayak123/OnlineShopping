2492561e33c5da8054b0695397a5a6b00eb65487
Tapas Nayak <EE210131@sasken.com>
2019-05-08 17:34:31 +0530

    Error Handling added

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-'mongodb+srv://online-shopping:QnlVQK8abeEe5Th2@shopping-clusture-nffdo.mongodb.net/shop';
+ 'mongodb+srv://online-shopping:QnlVQK8abeEe5Th2@shopping-clusture-nffdo.mongodb.net/shop';
 
 const app = express();
 const store = new MongoDBStore({
@@ -42,28 +42,39 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
-
-app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
 });
 
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if(!user){
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch(err =>{
+      next(new Error(err));
+     } );
+});
+
+
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
-
+app.use('/500',errorController.get500);
 app.use(errorController.get404);
+app.use((error,req,res,next)=>{
+  res.redirect('/500');
+})
+
 
 mongoose
   .connect(MONGODB_URI)
